3d5e4d340b27ac9fc2ea7e22320c249b0c72d234
Francisco Venturini <94072993+Fralpz@users.noreply.github.com>
2021-11-23 23:09:20 -0300

    Format code

--- bot/main.py
+++ bot/main.py
@@ -7,10 +7,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 prefix = os.getenv("prefix")
-client = commands.Bot(command_prefix = prefix , case_insensitive = True)
-
 token = os.getenv("token")
-    
+
+client = commands.Bot(command_prefix=prefix, case_insensitive=True)
+
+
 @client.event
 async def on_ready():
     print(
@@ -20,16 +21,16 @@ async def on_ready():
         f"---------------------------------------------------"
     )
     await client.change_presence(
-        activity=discord.Activity(
-            type=discord.ActivityType.listening, n=""
-        )
+        activity=discord.Activity(type=discord.ActivityType.listening, n="")
     )
+
 
 @client.command()
 async def dado(ctx, numero):
     if int(numero) <= 0:
-        await ctx.send('Põe um número acima de 0, burro')
-    roll = random.randint(1,int(numero))
-    await ctx.send(f'O número que saiu no dado foi: {roll}')
-    
-client.run(token)   
+        await ctx.send("Põe um número acima de 0, burro")
+    roll = random.randint(1, int(numero))
+    await ctx.send(f"O número que saiu no dado foi: {roll}")
+
+
+client.run(token)
