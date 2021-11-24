9aa4f61602b142be105d5fa78c200cfde883c756
Francisco Venturini <94072993+Fralpz@users.noreply.github.com>
2021-11-23 22:45:59 -0300

    Create main.py
    Gragas bot 1

---
+++ bot/main.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+prefix = "$"
+client = commands.Bot(command_prefix = prefix , case_insensitive = True)
+
+token = os.getenv("token")
+    
+@client.event
+async def on_ready():
+    print(
+        f"\n---------------------------------------------------\n"
+        f"Bot Ready!\n"
+        f"Current Prefix: {prefix}\n"
+        f"---------------------------------------------------"
+    )
+    await client.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.listening, n=""
+        )
+    )
+
+@client.command()
+async def dado(ctx, numero):
+    if int(numero) <= 0:
+        await ctx.send('Põe um número acima de 0, burro')
+    roll = random.randint(1,int(numero))
+    await ctx.send(f'O número que saiu no dado foi: {roll}')
+    
+client.run(token)   
