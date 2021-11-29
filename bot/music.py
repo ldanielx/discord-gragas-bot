85dc009708180edb1ab3b42ba1d1cd10ddb5b9db
ldanielx <lucasdanielbarradas@gmail.com>
2021-11-29 00:18:27 -0300

    Making the bonk sound effect when entering a voice chat

--- bot/music.py
+++ bot/music.py
@@ -89,6 +89,7 @@ class Music(commands.Cog, name="Music"):
         if len(self.song_queue[ctx.guild]) > 1:
             del self.song_queue[ctx.guild][0]
             run_coroutine_threadsafe(self.edit_message(ctx), self.bot.loop)
+
             voice.play(
                 FFmpegPCMAudio(
                     self.song_queue[ctx.guild][0]["source"], **Music.FFMPEG_OPTIONS
@@ -121,15 +122,27 @@ class Music(commands.Cog, name="Music"):
             voice = await channel.connect()
 
         await ctx.guild.me.edit(deafen=True)
-
         await ctx.message.delete()
+
         if not voice.is_playing():
-            self.song_queue[ctx.guild] = [song]
+            self.song_queue[ctx.guild] = [
+                {
+                    "embed": "",
+                    "source": "https://r4---sn-uxaxh5g-bpbed.googlevideo.com/videoplayback?expire=1638160228&ei=BAOkYcnPG7by1sQPyIaU0AM&ip=189.104.180.227&id=o-ABbG8hWAMlznYVc60PCDVB6qEYnEjtNviZIMlklm6jhR&itag=249&source=youtube&requiressl=yes&mh=v6&mm=31%2C29&mn=sn-uxaxh5g-bpbed%2Csn-uxaxh5g-jo4l&ms=au%2Crdu&mv=m&mvi=4&pl=18&initcwndbps=291250&vprv=1&mime=audio%2Fwebm&ns=T7wSslLfG0do3gu88weTw6QG&gir=yes&clen=3853&dur=1.861&lmt=1617063114950664&mt=1638138299&fvip=4&keepalive=yes&fexp=24001373%2C24007246&beids=23886210&c=WEB&txp=5311222&n=kDpeXptGjDKIhudF&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRAIgI6dsjXYdJYBo8f60iHQhldAHCcjNfRNh5zsyUCCSgTgCICiL0k-3lkjKjAyNfw7K8MWNywfN4MG5fHhOqdTmQP8I&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIgftEYsjJUf3IgEENmry9sVBBn0QMI5pt-15RbmSkJMlgCIQC2X7BWAHebHzN6P5a3PqRrQkYRL7bykI2qDMjjTID7bw%3D%3D",
+                    "title": "bonk",
+                }
+            ]
+            self.song_queue[ctx.guild].append(song)
             self.message[ctx.guild] = await ctx.send(embed=song["embed"])
+
             voice.play(
-                FFmpegPCMAudio(song["source"], **Music.FFMPEG_OPTIONS),
+                FFmpegPCMAudio(
+                    self.song_queue[ctx.guild][0]["source"],
+                    **Music.FFMPEG_OPTIONS,
+                ),
                 after=lambda e: self.play_next(ctx),
             )
+
             voice.is_playing()
         else:
             self.song_queue[ctx.guild].append(song)
